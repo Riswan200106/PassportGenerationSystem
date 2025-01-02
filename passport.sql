d6f397f672fb1c1eea37a49c00e62fd98af71de8
Riswan200106 <142514359+Riswan200106@users.noreply.github.com>
2025-01-02 18:00:00 +0530

    Add files via upload

---
+++ passport.sql
@@ -0,0 +1,192 @@
+CREATE DATABASE PassportGenerationSystem
+
+--table for signup
+CREATE TABLE Accounts (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    DateOfBirth DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    PhoneNumber VARCHAR(15) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    Address VARCHAR(255) NOT NULL,
+    State VARCHAR(50) NOT NULL,
+    City VARCHAR(50) NOT NULL,
+    Username VARCHAR(50) NOT NULL UNIQUE,
+    Password VARCHAR(50) NOT NULL,
+    Role VARCHAR(20) NOT NULL
+);
+
+--signup stored procedure
+ALTER PROCEDURE sp_Signup
+    @FirstName VARCHAR(50),
+    @LastName VARCHAR(50),
+    @DateOfBirth DATE,
+    @Gender VARCHAR(10),
+    @PhoneNumber VARCHAR(15),
+    @Email VARCHAR(100),
+    @Address VARCHAR(255),
+    @State VARCHAR(50),
+    @City VARCHAR(50),
+    @Username VARCHAR(50),
+    @Password VARCHAR(255),
+    @Role VARCHAR(20),
+    @ResultMessage VARCHAR(255) OUTPUT 
+
+AS
+BEGIN
+    -- Check if Username already exists
+    IF EXISTS (SELECT 1 FROM Accounts WHERE Username = @Username)
+    BEGIN
+        SET @ResultMessage = 'Username already exists.'
+        RETURN
+    END
+
+    -- Check if Email already exists
+    IF EXISTS (SELECT 1 FROM Accounts WHERE Email = @Email)
+    BEGIN
+        SET @ResultMessage = 'Email already registered.'
+        RETURN
+    END
+
+    -- Check if PhoneNumber already exists
+    IF EXISTS (SELECT 1 FROM Accounts WHERE PhoneNumber = @PhoneNumber)
+    BEGIN
+        SET @ResultMessage = 'Phone number already registered.'
+        RETURN
+    END
+
+    -- If all checks pass, proceed with inserting the new user
+    INSERT INTO Accounts (
+        FirstName, LastName, DateOfBirth, Gender, PhoneNumber, 
+        Email, Address, State, City, Username, Password, Role
+    )
+    VALUES (
+        @FirstName, @LastName, @DateOfBirth, @Gender, @PhoneNumber,
+        @Email, @Address, @State, @City, @Username, @Password, @Role
+    );
+
+    -- Set success message
+    SET @ResultMessage = 'Signup successful.'
+END;
+
+
+
+--get all users list
+CREATE PROCEDURE sp_GetAllUsers
+AS
+BEGIN
+    SELECT 
+        Id,
+        FirstName,
+        LastName,
+        DateOfBirth,
+        Gender,
+        PhoneNumber,
+        Email,
+        Address,
+        State,
+        City,
+        Username,
+		Password,
+        Role
+    FROM Accounts;
+END;
+
+
+exec sp_GetAllUsers
+
+--signin stored procedure
+ALTER PROCEDURE sp_SignIn
+    @Username VARCHAR(50)
+AS
+BEGIN
+    SELECT Id, FirstName, LastName, Password, Role
+    FROM Accounts
+    WHERE Username = @Username;
+END;
+
+
+EXEC sp_SignIn @Username = Risvanrichu, @Password = 12345
+
+
+--deletion of account stored procedure
+ALTER PROCEDURE sp_DeleteAccount
+    @Id INT
+AS
+BEGIN
+   -- Delete dependent records in PassportApplications
+    DELETE FROM PassportApplications WHERE UserId = @Id;
+
+    -- Delete user from Accounts table
+    DELETE FROM Accounts WHERE Id = @Id;
+END;
+
+
+-------------------------------------------------------------------------------------------------------------------
+
+--stored procedure for add new admins
+CREATE PROCEDURE sp_AddNewAdmin
+   @FirstName VARCHAR(50),
+    @LastName VARCHAR(50),
+    @DateOfBirth DATE,
+    @Gender VARCHAR(10),
+    @PhoneNumber VARCHAR(15),
+    @Email VARCHAR(100),
+    @Address VARCHAR(255),
+    @State VARCHAR(50),
+    @City VARCHAR(50),
+    @Username VARCHAR(50),
+    @Password VARCHAR(50),
+	@Role VARCHAR(20)
+
+AS
+BEGIN
+    INSERT INTO Accounts (
+        FirstName, LastName, DateOfBirth, Gender, PhoneNumber, 
+        Email, Address, State, City, Username, Password, Role
+    )
+    VALUES (
+        @FirstName, @LastName, @DateOfBirth, @Gender, @PhoneNumber,
+        @Email, @Address, @State, @City, @Username, @Password,@Role
+    );
+END;
+
+
+
+
+--contact us feedback
+CREATE TABLE Feedback (
+    FeedId INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL,
+    Email NVARCHAR(100) NOT NULL,
+    Message NVARCHAR(MAX) NOT NULL,
+    CreatedAt DATETIME DEFAULT GETDATE()
+);
+
+--adding feedbacks
+CREATE PROCEDURE sp_AddFeedback
+    @Name NVARCHAR(100),
+    @Email NVARCHAR(100),
+    @Message NVARCHAR(MAX)
+AS
+BEGIN
+    INSERT INTO Feedback (Name, Email, Message)
+    VALUES (@Name, @Email, @Message);
+END;
+
+CREATE PROCEDURE sp_GetAllFeedback
+AS
+BEGIN
+    SELECT FeedId, Name, Email, Message, CreatedAt FROM Feedback;
+END
+
+
+--deleting feedback 
+CREATE PROCEDURE sp_DeleteFeedback
+    @FeedId INT
+AS
+BEGIN
+    DELETE FROM Feedback WHERE FeedId = @FeedId;
+END;
+
---
+++ passportapplication.sql
@@ -0,0 +1,159 @@
+--Table for passport application
+CREATE TABLE PassportApplications (
+    AppID INT IDENTITY(1,1) PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    DateOfBirth DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    PhoneNumber VARCHAR(15) NOT NULL,
+    Email VARCHAR(100) NOT NULL,
+    Address VARCHAR(200) NOT NULL,
+    Nationality VARCHAR(50) NOT NULL,
+    State VARCHAR(50) NOT NULL,
+    City VARCHAR(50) NOT NULL,
+    PhotoBase64 VARCHAR(MAX) ,
+    DocumentBase64 VARCHAR(MAX),
+    GuardianName VARCHAR(100) NOT NULL,
+    MaritalStatus VARCHAR(20) NOT NULL,
+	Status VARCHAR(20) DEFAULT 'Pending'
+);
+
+-- Alter PassportApplications table to include UserId as foreign key
+ALTER TABLE PassportApplications
+ADD UserId INT;
+
+-- Assuming you have an Accounts table for users
+-- Add a foreign key constraint to link the UserId with the Accounts table
+ALTER TABLE PassportApplications
+ADD CONSTRAINT FK_User_Applications FOREIGN KEY (UserId) REFERENCES Accounts(Id);
+
+
+-- stored procedure for create application
+ALTER PROCEDURE sp_CreateApplication
+    @FirstName NVARCHAR(50),
+    @LastName NVARCHAR(50),
+    @DateOfBirth DATE,
+    @Gender NVARCHAR(50),
+    @PhoneNumber NVARCHAR(15),
+    @Email NVARCHAR(255),
+    @Address NVARCHAR(255),
+    @Nationality NVARCHAR(50),
+    @State NVARCHAR(50),
+    @City NVARCHAR(50),
+    @PhotoBase64 NVARCHAR(MAX) = NULL,
+    @DocumentBase64 NVARCHAR(MAX) = NULL,
+    @GuardianName NVARCHAR(50),
+    @MaritalStatus NVARCHAR(50),
+    @Status VARCHAR(20),
+    @UserId INT -- Add UserId parameter
+AS
+BEGIN
+    INSERT INTO PassportApplications (FirstName, LastName, DateOfBirth, Gender, PhoneNumber, Email, Address, Nationality, State, City, PhotoBase64, DocumentBase64, GuardianName, MaritalStatus, Status, UserId)
+    VALUES (@FirstName, @LastName, @DateOfBirth, @Gender, @PhoneNumber, @Email, @Address, @Nationality, @State, @City, @PhotoBase64, @DocumentBase64, @GuardianName, @MaritalStatus, @Status, @UserId);
+END;
+
+
+
+--stored procedure for get all aplication details
+ALTER PROCEDURE sp_GetAllApplicationList
+AS
+BEGIN
+	SELECT AppID,FirstName, LastName, DateOfBirth, Gender, PhoneNumber, Email, Address, Nationality, State, City, PhotoBase64, DocumentBase64, GuardianName, MaritalStatus,Status
+	FROM PassportApplications
+END
+
+
+--execution
+EXEC sp_GetAllApplicationList
+
+
+--stored procedure for view application by id
+ALTER PROCEDURE sp_ViewApplicationById
+    @AppID INT
+AS
+BEGIN
+    SELECT AppID, UserId, FirstName, LastName, DateOfBirth, Gender, PhoneNumber, 
+        Email, Address, Nationality, State, City, PhotoBase64, DocumentBase64, GuardianName, MaritalStatus, Status FROM PassportApplications WHERE AppID = @AppID;
+END; 
+
+--execution
+EXEC sp_ViewApplicationById @AppID = 1
+
+
+--update application stored procedure
+ALTER PROCEDURE sp_UpdateApplication
+    @AppID INT,
+    @FirstName NVARCHAR(100),
+    @LastName NVARCHAR(100),
+    @DateOfBirth DATE,
+    @Gender NVARCHAR(10),
+    @PhoneNumber NVARCHAR(15),
+    @Email NVARCHAR(100),
+    @Address NVARCHAR(MAX),
+    @Nationality NVARCHAR(50),
+    @State NVARCHAR(50),
+    @City NVARCHAR(50),
+    @PhotoBase64 NVARCHAR(MAX),
+    @DocumentBase64 NVARCHAR(MAX),
+    @GuardianName NVARCHAR(100),
+    @MaritalStatus NVARCHAR(20),
+    @Status NVARCHAR(20)
+
+AS
+BEGIN
+    UPDATE PassportApplications
+    SET 
+        FirstName = @FirstName,
+        LastName = @LastName,
+        DateOfBirth = @DateOfBirth,
+        Gender = @Gender,
+        PhoneNumber = @PhoneNumber,
+        Email = @Email,
+        Address = @Address,
+        Nationality = @Nationality,
+        State = @State,
+        City = @City,
+        PhotoBase64 = @PhotoBase64,
+        DocumentBase64 = @DocumentBase64,
+        GuardianName = @GuardianName,
+        MaritalStatus = @MaritalStatus,
+        Status = @Status
+    WHERE AppID = @AppID;
+END;
+
+--delete application stored procedure
+ALTER PROCEDURE sp_DeleteApplication
+    @AppID INT
+AS
+BEGIN
+    DELETE FROM PassportApplications WHERE AppID = @AppID;
+END;
+
+
+--stored procedure for update the status of the application
+CREATE PROCEDURE sp_UpdateApplicationStatus
+    @AppID INT,
+    @Status VARCHAR(20)
+AS
+BEGIN
+    UPDATE PassportApplications
+    SET Status = @Status
+    WHERE AppID = @AppID;
+END;
+
+
+
+-- Stored Procedure to get applications for a specific user
+ALTER PROCEDURE sp_GetApplicationsByUserId
+    @UserId INT
+AS
+BEGIN
+    SELECT AppID, FirstName, LastName, DateOfBirth, Gender, PhoneNumber, 
+        Email, Address, Nationality, State, City, PhotoBase64, DocumentBase64, GuardianName, MaritalStatus, Status FROM PassportApplications WHERE UserId = @UserId;
+END;
+
+---------------------------------------------------
+EXEC sp_GetApplicationsByUserId @UserId = 1;
+
+SELECT DISTINCT UserId FROM PassportApplications;
+
